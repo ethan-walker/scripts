0f294a154eca4acb683d8c8e5fc4b1382df5dd4d
tictac <101420077+tictac7126@users.noreply.github.com>
2022-11-30 19:23:50 +0000

    Create theme_toggle.js

---
+++ theme_toggle.js
@@ -0,0 +1,36 @@
+const theme_icon = document.querySelector(".theme_icon");
+function setTheme(theme) {
+	localStorage.setItem("theme", theme);
+	if (theme === "light") {
+		theme_icon.innerHTML = "dark_mode";
+		document.body.classList.remove("dark-theme");
+	}
+	else {
+		theme_icon.innerHTML = "light_mode";
+		
+		document.body.classList.add("dark-theme");
+	}
+}
+
+function toggleTheme() {
+	theme = localStorage.getItem("theme");
+	if (theme === "light") {
+		setTheme("dark");
+	}
+	else {
+		setTheme("light");
+	}
+}
+
+function loadTheme() {
+	theme = localStorage.getItem("theme");
+	if (theme === "light") {
+		setTheme("light");
+	}
+	else if (theme === "dark") {
+		setTheme("dark");
+	}
+	else {
+		setTheme("light");
+	}
+}
